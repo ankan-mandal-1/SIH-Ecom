11acc5120b0a12f3410575dceb490135d8cbfda1
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2024-09-06 11:26:12 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <p>Design inside this box Container</p>
+    </div>
+  )
+}
+
+export default App
